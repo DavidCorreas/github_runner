3af38bb6ab339334074a9a125010efb7f35fc605
DavidCorreas <davidestu77@gmail.com>
2023-11-14 14:02:53 +0000

    Fix: runner kubernetes

--- bh_orchestrator_runner/deploy_all.bash
+++ bh_orchestrator_runner/deploy_all.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-eval $(minikube docker-env)
+# eval $(minikube docker-env)
 
 # Create the secret for the github token
 kubectl apply -f github-secret.yaml
 
 # Build the image. This generates image=github-runner-orchestrator
-docker compose build
-# minikube image load github-runner-orchestrator
+cp -r /home/david/.minikube/certs ./certs
+docker compose build # No minikube env. Need to copy files from host
+minikube image load github-runner-orchestrator
 
 # Deploy the orchestrator
 kubectl apply -f runner-manifest.yaml
